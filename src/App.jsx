7ec2d48c24b4b2afe97cd0c84a103f50e9b13159
nishantcm <cmnishantwork@gmail.com>
2024-06-18 00:28:27 +0530

    Update both the files

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import Log from './components/Log.jsx';
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
 
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function deriveActivePlayer(gameTurns){
   let currentPlayer = 'X';
 
@@ -22,7 +28,29 @@ function App() {
   //const [activePlayer, setActivePlayer] = useState('X');
 
   const activePlayer = deriveActivePlayer(gameTurns);
-  
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square;
+
+      gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol = gameBoard[combination[0]. row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1]. row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2]. row][combination[2].column];
+
+    if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+
+    } {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     //setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
@@ -45,9 +73,10 @@ function App() {
         <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
         <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
       </ol>
+      {winner && <p>You won, {winner}!</p>}
       <GameBoard 
         onSelectSquare={handleSelectSquare}
-        turns={gameTurns}
+        board={gameBoard}
       />
     </div>
     <Log turns={gameTurns} />
