0ffa6f8421ad58976125637c3e85998f88dc1455
nishantcm <cmnishantwork@gmail.com>
2024-10-16 23:57:37 +0530

    Update the files

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,10 @@ function deriveActivePlayer(gameTurns){
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    X: 'Player 1',
+    O: 'Player 2',
+  });
   const [gameTurns, setGameTurns] = useState([]);
   //const [hasWinner, setHasWinner] = useState(false);
   //const [activePlayer, setActivePlayer] = useState('X');
@@ -75,12 +79,31 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prePlayers => {
+      return {
+        ...prePlayers,
+        [symbol]: newName
+      };
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+          <Player 
+            initialName="Player 1" 
+            symbol="X" 
+            isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player 
+            initialName="Player 2"
+            symbol="O" 
+            isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
         {(winner || hasDraw) && (
           <GameOver winner={winner} onRestart={handleRestart} />
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,28 +1,29 @@
 import { useState } from "react";
 
-export default function Player ({initialName, symbol, isActive}){
-    const [ playerName, setPlayerName] = useState(initialName);
-    const [ isEditing, setIsEditing ] = useState(false);
+export default function Player ({ initialName, symbol, isActive, onChangeName }){
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
       setIsEditing((editing) => !editing);
+      onChangeName(symbol, playerName);
     }
     
-    function handleChange(event){
+    function handleChange(event) {
        setPlayerName(event.target.value);
     }
 
     let editablePlayerName = <span className="player-name">{playerName}</span>;
     //let btnCaption = 'Edit';
 
-    if(isEditing) {
+    if (isEditing) {
       editablePlayerName = (
         <input type="text" required value={playerName} onChange={handleChange} />
       );
       //btnCaption = 'Save';
     }
 
-    return(
+    return (
         <li class={isActive ? 'active' : undefined}>
           <span className="player">
             {editablePlayerName}
